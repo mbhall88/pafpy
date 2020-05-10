4fab1ab0393e3b0f9c67b7e205c547669619538a
Michael Hall <michael@mbh.sh>
2020-05-10 18:46:51 +1000

    test all from_str cases

--- tests/test_paf.py
+++ tests/test_paf.py
@@ -8,3 +8,83 @@ class TestFromStr:
 
         with pytest.raises(MalformattedRecord):
             PafRecord.from_str(line)
+
+    def test_line_has_too_few_fields_raises_error(self):
+        line = "qname\t1\t3"
+
+        with pytest.raises(MalformattedRecord):
+            PafRecord.from_str(line)
+
+    def test_line_with_no_tags(self):
+        fields = [
+            "query_name",
+            "1239",
+            "65",
+            "1239",
+            "+",
+            "target_name",
+            "4378340",
+            "2555250",
+            "2556472",
+            "1139",
+            "1228",
+            "60",
+        ]
+        line = "\t".join(fields)
+
+        actual = PafRecord.from_str(line)
+        expected = PafRecord(
+            "query_name",
+            1239,
+            65,
+            1239,
+            Strand.Forward,
+            "target_name",
+            4378340,
+            2555250,
+            2556472,
+            1139,
+            1228,
+            60,
+            [],
+        )
+
+        assert actual == expected
+
+    def test_line_with_tags(self):
+        fields = [
+            "query_name",
+            "1239",
+            "65",
+            "1239",
+            "-",
+            "target_name",
+            "4378340",
+            "2555250",
+            "2556472",
+            "1139",
+            "1228",
+            "60",
+            "NM:i:89",
+            "ms:i:1906",
+        ]
+        line = "\t".join(fields)
+
+        actual = PafRecord.from_str(line)
+        expected = PafRecord(
+            "query_name",
+            1239,
+            65,
+            1239,
+            Strand.Reverse,
+            "target_name",
+            4378340,
+            2555250,
+            2556472,
+            1139,
+            1228,
+            60,
+            ["NM:i:89", "ms:i:1906"],
+        )
+
+        assert actual == expected
