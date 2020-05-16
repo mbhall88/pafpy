5581864cdb57611f17ece464e48de794316aede2
Michael Hall <michael@mbh.sh>
2020-05-16 21:39:04 +1000

    dont rely on poetry to get version

--- pafpy/__version__.py
+++ pafpy/__version__.py
@@ -1,14 +1,1 @@
-import re
-import subprocess
-
-
-def get_version() -> str:
-    result = subprocess.run(["poetry", "version"], stdout=subprocess.PIPE)
-    regex = re.compile(r"(?P<version>\d.\d.\d)")
-    match = regex.search(result.stdout.decode())
-    if match is None:
-        return "Could not retrieve version number."
-    return match.group("version")
-
-
-__version__ = get_version()
+__version__ = "0.1.1"
